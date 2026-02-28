c9d1cc1cb252aff56cc8cee5a5bbb056cff31b11
lcvriend <43742775+lcvriend@users.noreply.github.com>
2026-02-28 20:20:18 +0100

    feat: add dirty state styling

--- nuttige_links/static/js/components/container-group.js
+++ nuttige_links/static/js/components/container-group.js
@@ -19,11 +19,20 @@ export class ContainerGroup extends HTMLElement {
     }
 
     get record() {
-        return store.state[this.table].records[this.recordId]
+        return store.state[this.table].drafts[this.recordId]
+            ?? store.state[this.table].records[this.recordId]
     }
 
     get collapsed() {
         return store.state[this.table].uiCollapsed.has(this.recordId)
+    }
+
+    syncDirtyState() {
+        const { records, drafts, pendingDeletes } = store.state[this.table]
+        const id = this.recordId
+        this.toggleAttribute('data-dirty', id in drafts && id in records)
+        this.toggleAttribute('data-new', id in drafts && !(id in records))
+        this.toggleAttribute('data-pending-delete', pendingDeletes.has(id))
     }
 
     // DOM
@@ -56,12 +65,15 @@ export class ContainerGroup extends HTMLElement {
 
         this.unsubs = [
             store.subscribe(
-                s => s[this.table].records[this.recordId],
+                s => s[this.table].drafts[this.recordId] ?? s[this.table].records[this.recordId],
                 () => this.renderHeader()
             ),
             store.subscribe(
                 s => s[this.childTable].records,
-                () => this.buildChildren()
+                () => {
+                    this.buildChildren()
+                    this.toggleAttribute('data-empty', this.childContainer.children.length === 0)
+                }
             ),
             store.subscribe(
                 s => s[this.table].uiCollapsed.has(this.recordId),
@@ -73,8 +85,17 @@ export class ContainerGroup extends HTMLElement {
                 s => s[this.table].uiVisible,
                 (vis) => { this.hidden = vis ? !vis.has(this.recordId) : false }
             ),
+            store.subscribe(
+                s => s[this.table].drafts[this.recordId],
+                () => this.syncDirtyState()
+            ),
+            store.subscribe(
+                s => s[this.table].pendingDeletes,
+                () => this.syncDirtyState()
+            ),
         ]
 
+        this.syncDirtyState()
         this.renderHeader()
         this.buildChildren()
         this.setupListeners()
@@ -88,6 +109,7 @@ export class ContainerGroup extends HTMLElement {
         this.header.addEventListener('click', this.handleToggle)
     }
 
+    // render
     renderHeader() {
         if (!this.record) return
         this.header.textContent = this.record.name
--- nuttige_links/static/js/components/link-item.js
+++ nuttige_links/static/js/components/link-item.js
@@ -1,26 +1,49 @@
 import { store } from '../datastore.js'
 
 class LinkItem extends HTMLElement {
+    table = 'links'
+
+    // state
     get recordId() { return this.getAttribute('record-id') }
 
     get record() {
-        return store.state.links.records[this.recordId]
+        return store.state[this.table].drafts[this.recordId]
+            ?? store.state[this.table].records[this.recordId]
     }
 
+    syncDirtyState() {
+        const { records, drafts, pendingDeletes } = store.state.links
+        const id = this.recordId
+        this.toggleAttribute('data-dirty', id in drafts && id in records)
+        this.toggleAttribute('data-new', id in drafts && !(id in records))
+        this.toggleAttribute('data-pending-delete', pendingDeletes.has(id))
+    }
+
+    // life-cycle
     connectedCallback() {
         this.setAttribute('data-tree-item', '')
         this.setAttribute('data-tree-depth', '2')
 
         this.unsubs = [
             store.subscribe(
-                s => s.links.records[this.recordId],
+                s => s.links.drafts[this.recordId] ?? s.links.records[this.recordId],
                 () => this.render()
             ),
             store.subscribe(
-                s => s.links.uiVisible,
+                s => s[this.table].uiVisible,
                 (vis) => { this.hidden = vis ? !vis.has(this.recordId) : false }
             ),
+            store.subscribe(
+                s => s.links.drafts[this.recordId],
+                () => this.syncDirtyState()
+            ),
+            store.subscribe(
+                s => s.links.pendingDeletes,
+                () => this.syncDirtyState()
+            ),
         ]
+
+        this.syncDirtyState()
         this.render()
     }
 
@@ -28,6 +51,7 @@ class LinkItem extends HTMLElement {
         this.unsubs?.forEach(fn => fn())
     }
 
+    // render
     render() {
         if (!this.record) return
 
